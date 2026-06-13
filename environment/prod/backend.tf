71cf67f2702cd8871a9e685abf7ef6dcc0a17a6a
Mac <mcleonard.o.d@gmail.com>
2026-06-13 20:25:41 +0100

    Add Terraform remote backend configuration

---
+++ environment/prod/backend.tf
@@ -0,0 +1,9 @@
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "rg-netflix-tfstate-uksouth"
+    storage_account_name = "tfstatenetflixmac01"
+    container_name       = "tfstate"
+    key                  = "prod.terraform.tfstate"
+    use_azuread_auth     = true
+  }
+}
